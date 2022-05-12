49f0f0dd1c2a6cda19252d78220b4636389e3216
opeokunnuga <103292171+opeokunnuga@users.noreply.github.com>
2022-05-12 23:46:53 +0100

    Update Jenkinsfilefors3auto.groovy

--- Jenkinsfilefors3auto.groovy
+++ Jenkinsfilefors3auto.groovy
@@ -4,7 +4,7 @@ pipeline {
   stages {
     stage('Submit Stack') {
       steps {
-        sh "aws cloudformation create-stack --stack-name s3bucket --template-body file://S3bucketauto.yml --region 'eu-west-2'"
+        sh "aws cloudformation create-stack --stack-name s3bucket --template-body file://s3bucketauto.yml --region 'eu-west-2'"
       }
     }
   }
